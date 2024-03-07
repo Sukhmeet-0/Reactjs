cee71b953210ee7360d64f18d65ff64762cde636
Sukhmeet-0 <sukhmeets111@gmail.com>
2024-03-07 06:51:26 -0800

    adding hooks

--- 01vite-project/src/App.jsx
+++ 01vite-project/src/App.jsx
@@ -1,14 +1,28 @@
-import Chai from "./chai"
+import Chai from "./chai";
+import { useState } from "react";
 
 function App() {
-  
 
+  let [counter,setCounter] = useState(0)
+
+  // let counter = 0;
+  const addValue = () => {
+    // counter = counter + 1;
+    setCounter(counter+1);
+  };
+  const removeValue = () => {
+    // counter = counter - 1;
+    setCounter(counter-1);
+  };
   return (
     <>
-    <Chai/>
-    <h1>Kidaa sohneoo ki chaiidaaaa</h1>
+      <h1>Kidaa sohneoo ki chaiidaaaa</h1>
+      <h1>Counter value: {counter}</h1>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>remove Value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
